c235fd67e52e976a09ec0ad368be0e579ff0f35b
Sami Säily <>
2021-04-30 15:00:07 +0300

    add support for remote user credentials

--- src/cmd/highlander/main.go
+++ src/cmd/highlander/main.go
@@ -10,7 +10,25 @@ import (
 )
 
 var cmd Cmd
-var version = "0.1.0"
+var version = "0.1.1"
+
+// like httputil.NewSingleHostReverseProxy but with BasicAuth and Host header rewrite
+func NewSingleHostAuthReverseProxy(target *url.URL, user string, pass string) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		if _, ok := req.Header["User-Agent"]; !ok {
+			req.Header.Set("User-Agent", "") // no default agent
+		}
+
+		// extensions to NewSingleHostReverseProxy:
+		req.SetBasicAuth(user, pass)
+		req.Host = target.Host
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
 
 func main() {
 	cmd = parseCmd()
@@ -18,6 +36,9 @@ func main() {
 	log.Println("Highander v", version, " is now starting. (use -h for help)")
 	log.Println("binding address: ", cmd.bind)
 	log.Println("remote address: ", cmd.remote)
+	if cmd.remoteUser != "" {
+		log.Println("remote user", cmd.remoteUser)
+	}
 	log.Println("health tick: ", cmd.healthTick.String())
 	log.Println("health expiry: ", cmd.healthExpiry.String())
 	log.Println("instance ID: ", cmd.instanceID)
@@ -60,16 +81,22 @@ func main() {
 	}()
 
 	// then bind the service
-	h := httputil.NewSingleHostReverseProxy(u)
-	h.Transport = NewHighlanderProxy(
+	var proxy *httputil.ReverseProxy
+	if cmd.remoteUser != "" {
+		log.Printf("using authentication for user %s", cmd.remoteUser)
+		proxy = NewSingleHostAuthReverseProxy(u, cmd.remoteUser, cmd.remotePassword)
+	} else {
+		proxy = httputil.NewSingleHostReverseProxy(u)
+	}
+	proxy.Transport = NewHighlanderProxy(
 		cmd.healthTick,
 		cmd.healthExpiry,
 		func() bool {
 			return isLeader
 		})
-
-	err = http.ListenAndServe(cmd.bind, h)
+	err = http.ListenAndServe(cmd.bind, proxy)
 	if err != nil {
 		panic(err)
 	}
+
 }
